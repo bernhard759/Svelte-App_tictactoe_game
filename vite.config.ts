37e5c1b3622c13ccb092e7ac2f496c4b9d7714bc
bernhard759 <bernhard.gailer@gmx.de>
2023-03-07 11:16:18 +0100

    update vite config for gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Svelte-App_tictactoe_game/",
   plugins: [svelte()],
 })
